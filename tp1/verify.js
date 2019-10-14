2455ea3a7807b463dc59678a1422c4ef241933c2
Anthony Domingue <contact@anthonydomingue.com>
2019-10-14 21:37:35 +0200

    fix(tp1): more verbose when requiring exos

--- tp1/verify.js
+++ tp1/verify.js
@@ -1,34 +1,60 @@
-exo1 = require("./1")
-exo2 = require("./2")
-exo3 = require("./3")
+const process = require('process')
+const reset = '\x1b[0m'
+const red = '\x1b[31m'
+const blink = '\x1b[5m'
+const green = '\x1b[32m'
+const bright = '\x1b[1m'
 
-const reset = "\x1b[0m"
-const red = "\x1b[31m"
-const blink = "\x1b[5m"
-const green = "\x1b[32m"
-const bright = "\x1b[1m"
-const exos = [exo1, exo2, exo3]
+const exos = []
+const exoPaths = ['./1', './2', './3']
+
+const log = (messages) => {
+  if (!Array.isArray(messages)) {
+    console.log(messages)
+    return
+  }
+  for (const message of messages) {
+    console.log(message)
+  }
+}
+
+const verboseRequire = (path) => {
+  try {
+    return require(path)
+  } catch (err) {
+    log([`${bright}${red}Error when requiring ${path}/index.js${reset}`, err])
+    process.exit(1)
+  }
+}
+
+for (const exoPath of exoPaths) {
+  exos.push(verboseRequire(exoPath))
+}
 
 for (const exo of exos) {
-  console.log(`Starting test for ${exo.name}...`)
+  log(`Starting test for ${exo.name}...`)
   for (const dataset of exo.datasets) {
 
     let result = undefined
     try {
       result = exo.algo(dataset.input)
     } catch (err) {
-      console.log(`${exo.name} -> ${bright}${blink}${red}KO${reset}`)
-      console.log("\nError :")
-      console.log(err)
-      return
+      log([
+        `${exo.name} -> ${bright}${blink}${red}KO${reset}`,
+        "\nError :",
+        err
+      ])
+      process.exit(1)
     }
 
     if (result !== dataset.output) {
-      console.log(`${exo.name} -> ${bright}${blink}${red}KO${reset}`)
-      console.log(`${bright}Got ${result} but expected ${dataset.output}${reset}`)
-      return
+      log([
+        `${exo.name} -> ${bright}${blink}${red}KO${reset}`,
+        `${bright}Got ${result} but expected ${dataset.output}${reset}`
+      ])
+      process.exit(2)
     }
   }
-  console.log(`${exo.name} -> ${bright}${green}OK${reset}\n`)
+  log(`${exo.name} -> ${bright}${green}OK${reset}\n`)
 }
-console.log(`${bright}${green}GG !${reset}`)
+log(`${bright}${green}GG !${reset}`)
