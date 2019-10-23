551a15f4d78db36488987d11656679c60ed915aa
gabin <gdpre@MacBook-Pro-de-gabin.local>
2019-10-23 11:16:10 +0200

    UPD script

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,7 +7,23 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+    res = 0
+    let check = input[0]
+    for (let index = 1; index < input.length; index++) {
+        const ok = input[index]
+        let check2 = ok.split(' ')
+        check += parseInt(check2[0]);
+        check -= parseInt(check2[1]); 
+    }
 
+    if (check >= 0 && check <= 100) {
+      res = 1000;
+    } else if (check > 100 && check <= 10000) {
+      res = 100;
+    } else {
+        return "KO";
+    }
+    return res
     // AND HERE
   },
   verify: function (dataset, output) {
--- tp1/2/index.js
+++ tp1/2/index.js
@@ -7,7 +7,30 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+    nbrCarton = input[0]
+    res = 1
+    poid = 0
+    for (let index= 1; index < input.length; index++) {
+      const check = poid + input[index]
+      if (check == 100) {
+        poid = 0
+        if (input[index+1] != undefined) {
+          res += 1
+        }
+      } else if (check > 100) {
+        poid = input[index];
+        res+= 1
+        console.log("okok")
+      } else {
+        poid += input[index]
+      }
+      console.log(poid)
+    }
 
+    console.log(poid)
+    console.log(res)
+
+    return res
     // AND HERE
   },
   verify: function (dataset, output) {
